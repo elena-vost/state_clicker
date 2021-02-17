36a5c5b41da8237d4389581a98d5b1d4b06c441f
elena-vost <angara1298@gmail.com>
2021-02-17 15:37:23 +0100

    clicker done

--- src/Clicker.js
+++ src/Clicker.js
@@ -4,11 +4,20 @@ class Clicker extends Component {
     constructor(props){
         super(props);
         this.state = {num: 1};
+        this.genRandom = this.genRandom.bind(this);
+    }
+    genRandom(){
+        let rand = Math.floor(Math.random() *10) +1;
+        this.setState({num: rand});
     }
     render () {
         return (
             <div>
-        <h1>Number is: {this.state.num} </h1>
+                <h1>Nimber is {this.state.num} </h1>
+                {this.state.num === 7 
+                ? <h2>YOU WIN!</h2>
+                : <button onClick={this.genRandom} >Random Number</button>}
+               
         </div>
         );
     }
